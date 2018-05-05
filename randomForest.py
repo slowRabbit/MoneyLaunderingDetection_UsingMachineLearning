b6f82519878f5fea7cf879dcdc2ae7d8232d71b1
chetanmann95@gmail.com <chetanmann95@gmail.com>
2018-05-05 20:43:26 +0530

    extracting best features for 3D representation using random forest

--- randomForest.py
+++ randomForest.py
@@ -1,2 +1,94 @@
-# -*- coding: utf-8 -*-
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import random
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import LinearSVC
+from imblearn.under_sampling import NearMiss
+from imblearn import over_sampling as os
+from imblearn.pipeline import make_pipeline
+from imblearn.combine import SMOTETomek
+from imblearn.over_sampling import ADASYN
+from imblearn.under_sampling import ClusterCentroids
+from imblearn.over_sampling import RandomOverSampler
 
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import datasets
+from sklearn.feature_selection import SelectFromModel
+from sklearn.metrics import accuracy_score
+
+
+random.seed(50)
+
+# Importing the dataset
+dataset = pd.read_csv('/media/cyris/Studies/Hackathons/CodeGrind17/Techfest April 17th/Datasets/kaggle/kaggleData.csv')
+dataset.drop('nameOrig', axis=1, inplace=True)
+dataset.drop('nameDest', axis=1, inplace=True)
+dataset.drop('isFlaggedFraud', axis=1, inplace=True)
+
+#now data has this format
+#step,type,amount,oldbalanceOrg,newbalanceOrig,oldbalanceDest,newbalanceDest,isFraud,
+# 0 , 1   , 2   ,     3      ,      4       ,      5      ,      6       ,   7   ,    
+
+sample_dataframe = dataset.sample(n=100000)
+X = sample_dataframe.iloc[:, :-1].values
+y = sample_dataframe.iloc[:, 7].values
+
+print(sample_dataframe.isFraud.value_counts())
+
+
+# Encoding categorical data
+labelencoder = LabelEncoder()
+X[:, 1] = labelencoder.fit_transform(X[:, 1])
+onehotencoder = OneHotEncoder(categorical_features = [1])
+X = onehotencoder.fit_transform(X).toarray()
+
+# Avoiding the Dummy Variable Trap
+X = X[:, 1:]
+
+
+# Splitting the dataset into the Training set and Test set
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
+X_test, X_val, y_test, y_val = train_test_split(X_test, y_test, test_size=0.5, random_state=1)
+
+counts = np.unique(y_train, return_counts=True)
+
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_val = sc.transform(X_val)
+X_test = sc.transform(X_test)
+print(counts)
+
+
+# Apply the sampling
+#ada = ADASYN()
+#X_resampled, y_resampled = ada.fit_sample(X_train, y_train)
+#count = np.unique(y_resampled, return_counts=True)
+
+# Create a pipeline
+
+#pipeline4 = make_pipeline(ADASYN(),LinearSVC(random_state=1))
+#pipeline4.fit(X_train, y_train)
+#print(count)
+
+#predicted=pipeline4.predict(X_test)
+
+feat_labels = ['step','type','amount','oldbalanceOrg','newbalanceOrig',
+               'oldbalanceDest','newbalanceDest','isFraud']
+
+# Create a random forest classifier
+clf = RandomForestClassifier(n_estimators=100000, random_state=0, n_jobs=-1)
+
+# Train the classifier
+clf.fit(X_train, y_train)
+
+# Print the name and gini importance of each feature
+for feature in zip(feat_labels, clf.feature_importances_):
+    print(feature)
+
+
+#y_test, pipeline4.predict(X_test))
+#print("Accuracy :",accuracy_score(y_test, predicted))
